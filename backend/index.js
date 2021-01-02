9a98f2292c38288b23a2b49a41164029d485f40b
David Gambo <dfa.gambo@gmail.com>
2021-01-02 15:31:23 +0100

    Indentation and added some callback

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,35 @@
-var AWS = require("aws-sdk");
-var DynamoDB = new AWS.DynamoDB.DocumentClient();
+const AWS = require("aws-sdk");
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-var params = {
-    TableName: "agenfit",
-    Item: {
-        "Timestamp": "2021-01-02",
-        "Type": "AMRAP",
-        "Timecap": 0,
-        "WOD": [
-          {
-            "exercice": "push-up",
-            "reps": 0
-          }
-          
-        ],
-        "Score" : "10 (RX)"
-      }      
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    const params = {
+        TableName: "agenfit",
+        Item: {
+            "Timestamp": "2021-01-02",
+            "Type": "AMRAP",
+            "Timecap": 0,
+            "WOD": [
+                {
+                    "exercice": "push-up",
+                    "reps": 0
+                }
+                
+            ],
+            "Score" : "10 (RX)"
+        }      
+    };
+    
+    DynamoDB.put(params, function (err, data) {
+        {
+            if (err) {
+                console.log(err);
+                callback(err);
+            }
+            else {
+                console.log(data);
+                callback(null, data);
+            }
+        }
+    });
 };
-
-DynamoDB.put(params, function (err) {
-    {
-            if (err) console.log(err);
-    }
-});
