3e5795a78827b5e5c732f73110651010fe12680e
David Gambo <dfa.gambo@gmail.com>
2021-01-02 16:02:56 +0100

    Parameters taken from input

--- backend/index.js
+++ backend/index.js
@@ -3,33 +3,28 @@ const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
     // TODO implement
-    const params = {
-        TableName: "agenfit",
-        Item: {
-            "Timestamp": "2021-01-02",
-            "Type": "AMRAP",
-            "Timecap": 0,
-            "WOD": [
-                {
-                    "exercice": "push-up",
-                    "reps": 0
-                }
-                
-            ],
-            "Score" : "10 (RX)"
-        }      
-    };
-    
-    DynamoDB.put(params, function (err, data) {
-        {
-            if (err) {
-                console.log(err);
-                callback(err);
-            }
-            else {
-                console.log(data);
-                callback(null, data);
-            }
+const params = {
+    TableName: "agenfit",
+    Item: {
+        "WodId" : "wod_" + Math.random(),
+        "Date": event.Date,
+        "Type": event.Type,
+        "Timecap": event.Timecap,
+        "WOD": event.WOD,
+        "Score" : event.Score
+      }      
+};
+
+DynamoDB.put(params, function (err, data) {
+    {
+        if (err) {
+        console.log(err);
+        callback(err);
         }
-    });
+        else {
+            console.log(data);
+            callback(null, data);
+        }
+    }
+});
 };
