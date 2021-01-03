f6d808376bfd189d30fdc371d6e640d91c13d620
David Gambo <dfa.gambo@gmail.com>
2021-01-03 13:57:06 +0100

    Added date in delete request

--- backend/agentfit-data/deletewod.js
+++ backend/agentfit-data/deletewod.js
@@ -3,9 +3,14 @@ const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
 module.exports.delete = (event, context, callback) => {
 
+
+//TODO : See later if the API must change (No more Date -> No more sort key. So DB must change also.)
 const params = {
         TableName: 'agenfit',
-        Key: event.pathParameters.woduuid
+        Key: {
+            "WodUuid": event.pathParameters.woduuid,
+            "WodDate": event.pathParameters.woddate,
+        }
 };
 
 DynamoDB.delete(params, function (err, data) {
